13b830ce4b530341ff876aeb95c1790269282d25
Teppei Nishi <>
2020-11-17 22:20:55 +0900

    no message

--- layouts/default.vue
+++ layouts/default.vue
@@ -65,8 +65,8 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          title: 'Vuetify',
+          to: '/vuetify',
         },
       ],
       miniVariant: false,
