61712eda66496d13198094d01b56617d71679cf2
Teppei Nishi <>
2020-11-22 00:30:40 +0900

    no message

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,50 +1,37 @@
-<template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-spacer />
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-app>
+<template lang="pug">
+v-app(dark)
+  v-navigation-drawer(
+    v-model='drawer',
+    :mini-variant='miniVariant',
+    :clipped='clipped',
+    fixed,
+    app
+  )
+    v-list
+      v-list-item(
+        v-for='(item, i) in items',
+        :key='i',
+        :to='item.to',
+        router,
+        exact
+      )
+        v-list-item-action
+          v-icon {{ item.icon }}
+        v-list-item-content
+          v-list-item-title(v-text='item.title')
+  v-app-bar(:clipped-left='clipped', fixed, app)
+    v-app-bar-nav-icon(@click.stop='drawer = !drawer')
+    v-spacer
+    v-toolbar-title(v-text='title')
+    v-spacer
+  v-main
+    v-container
+      nuxt
+  v-navigation-drawer(v-model='rightDrawer', :right='right', temporary, fixed)
+    v-list
+      v-list-item(@click.native='right = !right')
+        v-list-item-action
+          v-icon light mdi-repeat
 </template>
 
 <script>
@@ -81,12 +68,12 @@ export default {
           to: '/pixijs',
         },
         {
-          icon: 'mdi-square-outline',
+          icon: 'mdi-controller-classic',
           title: 'Phaser',
           to: '/phaser',
         },
         {
-          icon: 'mdi-square-outline',
+          icon: 'mdi-google-controller',
           title: 'Babylon.js',
           to: '/babylonjs',
         },
