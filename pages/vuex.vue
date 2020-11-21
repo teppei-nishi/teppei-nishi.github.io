931b287c42be9e70def584e3daa0aded75f00251
Teppei Nishi <>
2020-11-22 01:18:40 +0900

    no message

--- pages/vuex.vue
+++ pages/vuex.vue
@@ -1,14 +1,24 @@
 <template lang="pug">
 div
   h1 Vuex
-  .text-center {{ count }}
+  .text-center
+    | count: {{ count }}
+    br
+    | modal: {{ modal }}
+    br
+    | drawer: {{ drawer }}
   .text-center.mt-2
     v-dialog(v-model='modal', width='500', @input='(v) => v || hideModal()')
       template(v-slot:activator='{ on, attrs }')
         v-btn(v-bind='attrs', v-on='on') open modal
       v-card
         v-card-title Modal
-        v-card-text.text-center {{ count }}
+        v-card-text.text-center
+          | count: {{ count }}
+          br
+          | modal: {{ modal }}
+          br
+          | drawer: {{ drawer }}
         v-btn.mt-2(block, @click='increment') increment
         v-btn.mt-2(block, @click='reload') reload
         v-divider
@@ -33,6 +43,9 @@ export default {
       set() {
         this.$store.dispatch('showModal')
       },
+    },
+    drawer() {
+      return this.$store.state.drawer
     },
   },
   methods: {
