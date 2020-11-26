3dc3555d9df29c33494636a703ec3b6c1d412676
Teppei Nishi <>
2020-11-27 00:53:40 +0900

    no message

---
+++ components/BaseAccordion.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+.accordion
+  .header(@click='toggleAccordion') {{ header }}
+  .content
+    slot
+</template>
+
+<script>
+export default {
+  props: {
+    header: { default: '', type: String },
+  },
+  methods: {
+    toggleAccordion(e) {
+      const el = e.target
+      el.classList.toggle('active')
+      const content = el.nextElementSibling
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null
+      } else {
+        content.style.maxHeight = content.scrollHeight + 'px'
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+
+  &:hover {
+    background-color: #ccc;
+  }
+
+  &.active {
+    background-color: #ccc;
+  }
+}
+
+.content {
+  padding: 0 18px;
+  background-color: white;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s;
+  color: #444;
+}
+</style>
--- pages/accordion.vue
+++ pages/accordion.vue
@@ -2,66 +2,47 @@
 div
   h2 Animated Accordion
   p Click on the buttons to open the collapsible content.
-  .accordion(@click='toggleAccordion')
-    | Section 1
-  .panel
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  .accordion(@click='toggleAccordion')
-    | Section 2
-  .panel
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  .accordion(@click='toggleAccordion')
-    | Section 3
-  .panel
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  BaseAccordion(
+    v-for='item in accordions',
+    :key='item.id',
+    :header='item.header'
+  )
+    .accordion-content {{ item.content }}
 </template>
 
 <script>
+import BaseAccordion from '~/components/BaseAccordion.vue'
+
 export default {
-  methods: {
-    toggleAccordion(e) {
-      const el = e.target
-      el.classList.toggle('active')
-      const panel = el.nextElementSibling
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + 'px'
-      }
-    },
+  components: {
+    BaseAccordion,
+  },
+  data() {
+    return {
+      accordions: [
+        {
+          header: 'Section 1',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+        {
+          header: 'Section 2',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+        {
+          header: 'Section 3',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+      ],
+    }
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.accordion {
-  background-color: #eee;
-  color: #444;
-  cursor: pointer;
-  padding: 18px;
-  width: 100%;
-  border: none;
-  text-align: left;
-  outline: none;
-  font-size: 15px;
-  transition: 0.4s;
-}
-
-.active,
-.accordion:hover {
-  background-color: #ccc;
-}
-
-.panel {
-  padding: 0 18px;
-  background-color: white;
-  max-height: 0;
-  overflow: hidden;
-  transition: max-height 0.2s ease-out;
-  color: #444;
-
-  p {
-    margin-top: 1em;
-  }
+.accordion-content {
+  margin: 1em 0;
 }
 </style>
